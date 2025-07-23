42e2739750b70ae7cdbfbd6061f4816a2fd3728d
anonymousecode <anonymousecode1@gmail.com>
2025-07-23 10:34:31 +0530

    added modal for note input, added insert logic for note input

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -16,4 +16,19 @@ class NotesController extends Controller
 
         return view('dashboard', ['notes' => $notes]);
     }
+
+    function save(Request $request){
+        $email = Auth::user()->email;
+        $note = new Note();
+        $note->email = $email;
+        $note->title = $request->input('title');
+        $note->content = $request->input('content');
+        $status = $note->save();
+        if($status){
+            return redirect()->route('dashboard')->with('success', 'Note saved successfully!');
+        } else {
+            return redirect()->route('dashboard')->with('error', 'Failed to save note.');
+        }
+
+    }
 }
